58ee2ad09bbb0d64632610f74231c4f5c73d9698
miharbm <m.borodin@inlinegroup.ru>
2025-04-04 21:37:19 +0300

    add clean up tables

--- backend/src/main/java/ru/backend/controller/ImportController.java
+++ backend/src/main/java/ru/backend/controller/ImportController.java
@@ -1,11 +1,13 @@
 package ru.backend.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.backend.service.DatabaseCleanupService;
 import ru.backend.service.ImportService;
 
 import java.io.IOException;
@@ -15,10 +17,14 @@ import java.io.IOException;
 @CrossOrigin(origins = "*")
 public class ImportController {
     private final ImportService importService;
+    private final DatabaseCleanupService databaseCleanupService;
 
-    public ImportController(ImportService importService) {
+    @Autowired
+    public ImportController(ImportService importService, DatabaseCleanupService databaseCleanupService) {
         this.importService = importService;
+        this.databaseCleanupService = databaseCleanupService;
     }
+
 
     @PostMapping("/general-menu")
     public ResponseEntity<Void> initGeneralMenu() throws IOException {
@@ -30,5 +36,11 @@ public class ImportController {
     public ResponseEntity<Void> initCombos() throws IOException {
         importService.importCombosFromJson();
         return ResponseEntity.status( HttpStatus.CREATED).build();
+    }
+
+    @PostMapping("/clean-up-tables")
+    public ResponseEntity<Void> cleanUpTables() {
+        databaseCleanupService.clearAllTables();
+        return ResponseEntity.status( HttpStatus.OK).build();
     }
 }
